f754fe2e798072c4394f4b2741e91474d18a8392
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-03-11 19:43:38 -0500

    logica login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from 'src/app/login/login.module';
@@ -13,6 +13,7 @@ import { LoginModule } from 'src/app/login/login.module';
     BrowserModule,
     LoginModule,
     AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+  password: string;
+
+  constructor(private router: Router) {
+    this.username = '';
+    this.password = '';
+  }
+
+  login(): void {
+    if (this.username === 'user1' && this.password === '12345') {
+      this.router.navigate(['home']);
+    } else {
+      alert('Usuario o contraseña no coinciden');
+    }
+  }
 
   ngOnInit() {
   }
